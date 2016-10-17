cb69e43d580ed389bd6d0dd175f7e210f09c5e83
Alexander Varney <alexandervarney@gmail.com>
2016-10-17 21:10:35 +0200

    use osascript to send keypress

--- mute.rb
+++ mute.rb
@@ -1,21 +1,8 @@
 require "rubygems"
 require "dream_cheeky"
-require "watir-webdriver"
 
 DreamCheeky::BigRedButton.run do
-  browser = Watir::Browser.new:chrome
-  browser.goto "https://hangouts.google.com"
-
   push do
-    muteButton = browser.element(:class, "ha-w-P-y-Xi-f").parent;
-    if muteButton.exists?
-      muteButton.click
-      puts "Mute button clicked"
-    else
-      puts "No mute button found"
-    end
+    system("osascript -e 'tell application \"System Events\" to keystroke \"d\" using {command down}'")
   end
 end
-
-# JS Alternative
-# browser.execute_script("var el = document.querySelector('.ha-w-P-y-Xi-f').parentNode; el.dispatchEvent(new MouseEvent('mousedown')); el.dispatchEvent(new MouseEvent('mouseup')); el.dispatchEvent(new MouseEvent('mouseout'));")
